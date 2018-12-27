f21bcd1e7282f080eaebd8a97151ac54dfc2a33d
Rajesh Yarasani <rajeshyarasani.nr@gmail.com>
2018-12-26 21:34:38 -0600

    Initial commit

---
+++ app/src/main/java/com/zrieola/findlogo/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.zrieola.findlogo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },1000);
+    }
+}
